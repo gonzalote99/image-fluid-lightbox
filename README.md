8f5d98e1dc71e69b495a80b9fd9c1a4b01505e03
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2023-12-11 15:01:19 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# image-fluid-lightbox
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/gonzalote99/image-fluid-lightbox)
---
+++ script.js
@@ -0,0 +1,20 @@
+const modal = document.querySelector('.modal');
+const previews = document.querySelectorAll('.gallery img');
+const original = document.querySelector('.full-img');
+const caption = document.querySelector('.caption');
+
+previews.forEach((preview) => {
+preview.addEventListener('click', () => {
+  modal.classList.add('open');
+  const originalSize = preview.getAttribute('data-original');
+  original.src = `https://source.unsplash.com/${originalSize}`;
+  caption.textContent = preview.alt;
+});
+});
+
+modal.addEventListener('click', (e) => {
+if(e.target.classList.contains('modal')) {
+  modal.classList.remove('open');
+  original.classList.remove('open');
+}
+})
